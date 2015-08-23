b5452d137923136eca651662f1c7cb248c7ca7a9
Piotr Szotkowski <chastell@chastell.net>
2015-08-23 19:13:20 +0200

    depend on rack-test and sinatra

--- gems.rb
+++ gems.rb
@@ -1,7 +1,10 @@
 source 'https://rubygems.org'
 
+gem 'sinatra', '~> 1.4'
+
 group :development do
-  gem 'minitest', '~> 5.7'
-  gem 'rerun',    '~> 0.10.0'
-  gem 'rubocop',  '~> 0.33.0'
+  gem 'minitest',  '~> 5.7'
+  gem 'rack-test', '~> 0.6.0'
+  gem 'rerun',     '~> 0.10.0'
+  gem 'rubocop',   '~> 0.33.0'
 end
