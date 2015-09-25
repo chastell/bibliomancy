89497db435775ee071c2c46831ddce7ce4bf5a7d
Piotr Szotkowski <chastell@chastell.net>
2015-09-25 16:35:05 +0200

    test_helper: use rack-test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
 ENV['MT_NO_EXPECTATIONS'] = 'true'
+ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'rack/test'
+
+module Minitest
+  class Spec
+    include Rack::Test::Methods
+  end
+end
