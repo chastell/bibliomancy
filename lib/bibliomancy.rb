dae035cd0cdb7c97d976f23fa94a4e948f3c7fb0
Piotr Szotkowski <chastell@chastell.net>
2015-09-25 17:15:11 +0200

    test_helper: setup Bogus

---
+++ lib/bibliomancy.rb
@@ -0,0 +1,1 @@
+Bibliomancy = Module.new
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,11 @@ ENV['MT_NO_EXPECTATIONS'] = 'true'
 ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bogus/minitest/spec'
 require 'rack/test'
+require_relative '../lib/bibliomancy'
+
+Bogus.configure { |config| config.search_modules << Bibliomancy }
 
 module Minitest
   class Spec
